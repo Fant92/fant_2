8f4697a28391542afa427f96fa8d7e5e4d9c37b1
Alfred van Kuik <alfredvankuik@gmail.com>
2012-08-14 16:10:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fant2::Application.config.secret_token = '554ae49d5bc1ffd1127a943409fe86a0b0d0db30ee6d3a60706b32dc7d64b70657d69972ee7f043ebe63168f21229125d34f6f16fb0436c6b21c8d8cc0f91d46'
